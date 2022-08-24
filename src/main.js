210ea9b0c3d695d076554ccb7e0f14c6658abf25
zhangxiang <zhangxiang@everisetech.com>
2022-08-24 17:16:17 +0800

    首页搭建

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
 import {createApp} from 'vue'
 import App from './App.vue'
-import Vant from 'vant'
+import router from '@/router'
+
+import Vant, { Lazyload } from 'vant'
 import  Http from "@/http/index";
 import 'vant/lib/index.css'
 import '@/assets/css/reset.css'
 import '@/assets/iconfont/iconfont.css'
 
 
-import router from '@/router'
-
 const app = createApp(App)
 app.config.globalProperties.$http = Http
 
-app.use(Vant).use(router)
+app.use(Vant).use(Lazyload).use(router)
 app.mount('#app')
--- src/views/book/Book.vue
+++ src/views/book/Book.vue
@@ -20,6 +20,85 @@
             @click="toSearch"
         />
       </van-sticky>
+      <van-loading v-show="loading" style="text-align: center;margin-top: 20px" type="spinner" color="#1989fa">加载中...
+      </van-loading>
+      <div v-if="detail.hot">
+        <van-sidebar>
+          <van-sidebar-item :title="detail.hot.name"/>
+        </van-sidebar>
+        <van-grid :column-num="2">
+          <van-grid-item v-for="item in detail.hot.list" :key="item.bookMenuUrl">
+            <div style="display: flex" @click="cellClick(item)">
+              <van-image
+                  width="80"
+                  height="100"
+                  lazy-load
+                  radius="10"
+                  :src="item.imgUrl"
+              />
+              <div style="flex: 1;max-height: 100px;margin-left: 8px">
+                <div style="font-size: 15px;line-height: 25px">{{ item.name }}</div>
+                <div style="font-size: 13px;line-height: 25px">{{ item.author }}</div>
+                <div style="font-size: 10px;line-height: 16px" class="van-multi-ellipsis--l3">
+                  {{ item.disc.trim() }}
+                </div>
+              </div>
+            </div>
+          </van-grid-item>
+        </van-grid>
+      </div>
+      <div v-if="detail.top">
+        <van-sidebar>
+          <van-sidebar-item :title="detail.top.name"/>
+        </van-sidebar>
+        <div style="border-bottom: 1px solid #eee"/>
+        <van-cell
+            v-for="(item,index) in detail.top.list"
+            :key="index"
+            :title="`${item.name}`"
+            :label="item.author"
+            :value="item.type "
+            @click="cellClick(item)"
+        />
+        <div style="border-bottom: 1px solid #eee"/>
+      </div>
+      <div v-if="detail.block" style="margin-top: 10px">
+        <van-tabs v-model="active" swipeable>
+          <van-tab v-for="(items,index) in detail.block.list" :key="index" :title="items.name">
+            <template v-for="item in items.list">
+              <div
+                  v-if="item.imgUrl"
+                  :key="item.bookMenuUrl"
+                  style="display: flex;padding: 5px 10px"
+                  @click="cellClick(item)"
+              >
+                <van-image
+                    width="66"
+                    height="80"
+                    radius="5"
+                    lazy-load
+                    :src="item.imgUrl"
+                />
+                <div style="flex: 1;max-height: 80px;margin-left: 8px">
+                  <div style="font-size: 15px;line-height: 22px">{{ item.name }}</div>
+                  <div style="font-size: 14px;line-height: 22px">{{ item.author }}</div>
+                  <div style="font-size: 12px;line-height: 20px" class="van-multi-ellipsis--l3">
+                    {{ item.disc.trim() }}
+                  </div>
+                </div>
+              </div>
+              <van-cell
+                  v-else
+                  :key="item.bookMenuUrl"
+                  :title="`${item.name}`"
+                  :label="item.author"
+                  :value="item.type "
+                  @click="cellClick(item)"
+              />
+            </template>
+          </van-tab>
+        </van-tabs>
+      </div>
     </van-pull-refresh>
   </div>
 </template>
@@ -34,14 +113,17 @@ const router = useRouter()
 const data = reactive({
   refreshing: false,
   detail: {},
-  loading: ""
+  loading: false,
+  active: 0
 })
 
-const {refreshing, detail} = toRefs(data)
+const {refreshing, detail, loading, active} = toRefs(data)
 
 const loadData = () => {
+  data.loading = true
   Http.get('/biquge/home').then(res => {
     data.refreshing = false
+    data.loading = false
     data.detail = res
   }).catch(() => {
     data.refreshing = false
@@ -51,6 +133,10 @@ const loadData = () => {
 
 const toSearch = () => {
   router.push({path: '/search'})
+}
+
+const cellClick = (item) => {
+  console.log(item)
 }
 
 onMounted(() => {
