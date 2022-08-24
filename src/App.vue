f25d4ad43f8b5df3a2e6caa1ae3d28afaff56172
zhangxiang <13283347501@163.com>
2022-08-24 23:06:04 +0800

    动画

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,77 @@
 <template>
   <div class="app">
-    <router-view></router-view>
+    <router-view v-slot="{ Component }">
+      <transition :name="transitionName">
+        <component :is="Component"/>
+      </transition>
+    </router-view>
   </div>
 </template>
 
 <script setup>
-import {watch} from 'vue'
+import {watch, reactive, toRefs} from 'vue'
 import {useRoute} from 'vue-router'
 
-const route = useRoute()
-watch(route, () => {
-  watch(() => route.meta, (n, o) => {
-        console.log('监听路由')
-        console.log('new:' + n + ',old:' + o)
-      }
-  )
+const data = reactive({
+  transitionName: ''
 })
+
+const {transitionName} = toRefs(data)
+
+const route = useRoute()
+
+watch(() => route.meta, (to, from) => {
+      console.log('------', to.index, from.index)
+      if (to.index && from.index) {
+        if (from.index <= to.index) {
+          data.transitionName = 'slide-left'
+        } else {
+          data.transitionName = 'slide-right'
+        }
+      }
+      console.log(data.transitionName)
+    }
+)
 </script>
 <style lang="less">
-.app {
+.slide-right-enter-active,
+.slide-right-leave-active,
+.slide-left-enter-active,
+.slide-left-leave-active,
+.fade-in-up-big-enter-active {
+  will-change: transform;
+  transition: all .5s;
+  position: absolute;
+  height: 100vh;
+  width: 100%;
+}
 
+.slide-right-enter {
+  opacity: 0;
+  transform: translate3d(-100%, 0, 0);
+}
+
+.slide-right-leave-to {
+  opacity: 0;
+  transform: translate3d(100%, 0, 0);
+}
+
+.slide-left-enter {
+  opacity: 0;
+  transform: translate3d(100%, 0, 0);
+}
+
+.slide-left-leave-to {
+  opacity: 0;
+  transform: translate3d(-100%, 0, 0);
+}
+
+.fade-in-up-big-enter {
+  opacity: 0;
+  transform: translate3d(0, 100%, 0);
+}
+
+.fade-in-up-big-leave-active {
+  transform: translate3d(0, -100%, 0);
 }
 </style>
